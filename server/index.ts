93cd3e2e1492b3aa0f297d8b79183d38f75d8bef
[dyad] <rafisha92@gmail.com>
2025-12-25 20:20:16 +0800

    Updated server/index.ts

--- server/index.ts
+++ server/index.ts
@@ -54,7 +54,7 @@ app.use((req, res, next) => {
     const message = err.message || "Internal Server Error";
 
     res.status(status).json({ message });
-    throw err;
+    return;
   });
 
   // importantly only setup vite in development and after
